9a8d8af91943432d29d7de1c6ae8f6f4ff6a4b6a
aqmebrak <aqmebrak@gmail.com>
2021-12-13 09:56:23 +0000

    upgrade tailwind + layout images

--- src/routes/gallery/index.svelte
+++ src/routes/gallery/index.svelte
@@ -5,8 +5,39 @@
 </script>
 
 
-<div class="m-4 grid gap-4 grid-cols-7">
+<div class="container">
     {#each images as image}
-        <img src={image} alt="img1"/>
+    <figure>
+        <img src={image} alt="img1"/>        
+    </figure>
     {/each}
 </div>
+
+
+<style>
+img {
+  max-width: 100%;
+  display: block;
+}
+
+figure {
+  margin: 0;
+  margin-top: 2rem;
+  display: grid;
+  grid-template-rows: 1fr auto;
+  break-inside: avoid;
+}
+
+figure > img {
+  grid-row: 1 / -1;
+  grid-column: 1;
+}
+
+.container {
+    margin: auto;
+  column-count: 3;
+  column-gap: 2rem;
+  margin-top: 4rem;
+}
+    
+</style>
