a031e9216abe006d7b416b3924e9437afd035e44
aqmebrak <aqmebrak@gmail.com>
2022-01-05 22:04:32 +0000

    add small analytics page counter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,8 @@
     <h1>Welcome to No1yz warehouse !</h1>
     <p>Here you can find all my creations, from music to paintings</p>
 </div>
-
+<!-- svelte-ignore a11y-missing-attribute -->
+<img src="https://app.piratepx.com/ship?p=f174aa78-4b94-4857-a367-cc1a1c89a61f" />
 
 <style>
     h1 {
