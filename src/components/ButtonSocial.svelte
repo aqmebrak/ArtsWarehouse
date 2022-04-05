b8991fd54d644e635bf307b1027843d9a79b763f
aqmebrak <aqmebrak@gmail.com>
2022-04-05 19:18:24 +0200

    social links + icons

---
+++ src/components/ButtonSocial.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import Icon from 'src/components/Icon.svelte';
+
+	export let icon;
+	export let label;
+	export let url;
+</script>
+
+<div class="flex justify-center mt-8">
+	<a href={url} target="_blank">
+		<Icon name={icon} className="h-4 w-4" />
+		<span class="pl-1">{label}</span>
+	</a>
+</div>
+
+<style>
+	a {
+		display: flex;
+		cursor: pointer;
+		flex-direction: row;
+		align-items: center;
+		background-color: var(--queen-pink);
+		color: var(--plump-purple);
+		border-radius: 5px;
+		border-width: 1px;
+		border-color: var(--plump-purple);
+		padding: 10px;
+		font-size: 14px;
+	}
+	a svg {
+		margin-bottom: 12px;
+	}
+
+</style>
