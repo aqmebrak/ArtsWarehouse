0d0b0b70f67b8349ac9f0644a1426ef496c497a2
aqmebrak <matthieu.perrin@decathlontravel.com>
2024-01-07 19:03:45 +0100

    improve bubble bounce

--- src/routes/respire/+page.svelte
+++ src/routes/respire/+page.svelte
@@ -2,42 +2,37 @@
 	import P5 from 'p5-svelte';
 	import type { Sketch } from 'p5-svelte';
 
-	let t = 0; // time variable
 	let directionUp = true;
-	const framerate = 30
-	const frameSteps = 1 / framerate;
+	let startTime = 0;
 
 	const sketch: Sketch = (p5) => {
-		const canvaH = window.innerHeight - (window.innerHeight * 0.2)
-		const offsetY = (canvaH - 20) / framerate * 5;  // 5 seconds of framerate per sec;
-		console.log(offsetY);
+		const canvaH = window.innerHeight - (window.innerHeight * 0.2);
+		const canvaW = window.innerWidth - (window.innerWidth * 0.5);
+		const circleWidth = 50
+		const circleHalfWidth = circleWidth /2;
 
 		p5.setup = () => {
-			p5.createCanvas(window.innerWidth - (window.innerWidth * 0.1), canvaH);
+			p5.createCanvas(canvaW, canvaH);
 			p5.noStroke();
 			p5.fill('#ed225d');
-			p5.background('#ebd7ef')
+			p5.background('#ebd7ef');
+			startTime = p5.millis(); // Record the start time
 		};
 
 		p5.draw = () => {
-			p5.frameRate(framerate);
-			p5.clear();
-			p5.background('#ebd7ef')
+			p5.background('#ebd7ef');
+			let elapsedTime = p5.millis() - startTime;
+			let initialPosY = directionUp ? (p5.height - circleHalfWidth) : circleHalfWidth;
 
-
-			let offset = directionUp ? -(t * offsetY) : (t * offsetY);
-			let initialPosY = directionUp ? p5.height - 40 : 40;
-			if(t > 5) {
+			// let offset = directionUp ? -(t * offsetY) : (t * offsetY);
+			if (elapsedTime >= 5000) {
 				directionUp = !directionUp;
-				t = 0;
+				startTime = p5.millis();
 			}
-			let bubbleX, bubbleY;
-			bubbleX = p5.width / 2;
-			bubbleY = initialPosY + offset;
 
-			p5.ellipse(bubbleX, bubbleY, 40, 40);
-
-			t = t + frameSteps; // update time
+			let bubbleX = p5.width / 2;
+			let bubbleY = initialPosY - ((p5.height - circleWidth ) * (elapsedTime / 5000)) * (directionUp ? 1 : -1); // 5000 milliseconds;
+			p5.ellipse(bubbleX, bubbleY, circleWidth, circleWidth);
 		};
 	};
 </script>
