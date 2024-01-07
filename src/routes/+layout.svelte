2c97ddfed0bc1808b109ab0ce7e0fdb222c9ac97
aqmebrak <matthieu.perrin@decathlontravel.com>
2024-01-07 14:22:02 +0100

    add vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
 	import 'src/app.css';
 	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
 
+	inject({ mode: dev ? 'development' : 'production' });
 	injectSpeedInsights();
 </script>
 
